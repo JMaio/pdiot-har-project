cb3b180467606b1767f82eade89f5e7fb62a4073
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-11-08 15:34:10 +0000

    [backend] create basic api for data logging

---
+++ backend/server.py
@@ -0,0 +1,72 @@
+# from models import RespeckModel
+from flask import Flask, request
+# from flask_restful import Resource , Api
+# from flask_restful_swagger_3 import Api, Resource, swagger
+from flask_restplus import fields, Resource, Api, reqparse
+# from werkzeug.exceptions import BadRequest
+
+
+# https://stackoverflow.com/a/22772916/9184658
+from collections import deque
+
+
+API_PREFIX = '/api/v1'
+# # https://pypi.org/project/flask-restful-swagger-3/
+# SWAGGER_URL = ''  # URL for exposing Swagger UI (without trailing '/')
+# API_URL = ''  # Our API url (can of course be a local resource)
+WINDOW_SIZE = 25
+
+app = Flask(__name__)
+api = Api(
+    app,
+    prefix=API_PREFIX,
+    # swagger_prefix_url=SWAGGER_URL,
+    # swagger_url=API_URL
+)
+
+data = {
+    "ABC": deque([[0]*WINDOW_SIZE], maxlen=WINDOW_SIZE)
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument('data', type=list, location='json', required=True)
+
+
+@api.route('/respeck/<string:respeck_mac>')
+class RespeckData(Resource):
+    @api.doc(responses={
+        200: 'Success',
+        400: 'Respeck not found'
+    })
+    def get(self, respeck_mac):
+        try:
+            r = {respeck_mac: data[respeck_mac]}
+            return r, 200
+        except KeyError:
+            return {}, 404
+
+    @api.expect({'data': fields.List(fields.List(fields.Float(required=True)))})
+    def post(self, respeck_mac):
+        args = parser.parse_args()
+        d = args['data']
+        print(args)
+        # print(d)
+        try:
+            data[respeck_mac].extend(d)
+        except KeyError:
+            data[respeck_mac] = deque(d, maxlen=WINDOW_SIZE)
+
+        return {
+            'mac': respeck_mac,
+            'data': list(data[respeck_mac])
+        }
+
+
+@api.route('/data')
+class FullData(Resource):
+    def get(self):
+        return {k: list(v) for (k, v) in data.items()}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
