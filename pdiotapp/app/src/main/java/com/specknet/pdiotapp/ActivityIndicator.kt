5123c8b11990021bbbe4b6b839433b7c0fd3193d
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-11-25 10:57:48 +0000

    [app] added movement / goal notifications (onClick timeline icons)

--- pdiotapp/app/src/main/java/com/specknet/pdiotapp/ActivityIndicator.kt
+++ pdiotapp/app/src/main/java/com/specknet/pdiotapp/ActivityIndicator.kt
@@ -1,17 +1,18 @@
 package com.specknet.pdiotapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
+import android.os.Build
 import android.util.AttributeSet
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.SeekBar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+private const val CHANNEL_ID = "PDIoT.ActivityNotificationChannel"
+private const val TAG = "ActivityNotification"
 
 /**
  * TODO: document your custom view class.
@@ -21,6 +22,9 @@ class ActivityIndicator @JvmOverloads constructor(
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
     private val activityLevelProgressIndicator: SeekBar
+    private val activityIndicatorIconSitting: ImageView
+    private val activityIndicatorIconStanding: ImageView
+    private val activityIndicatorIconRunning: ImageView
 
     init {
         inflate(context, R.layout.widget_activity_indicator, this)
@@ -36,8 +40,80 @@ class ActivityIndicator @JvmOverloads constructor(
                 true
             }
         }
+
+        activityIndicatorIconSitting = findViewById(R.id.activityIndicatorIconSitting)
+        activityIndicatorIconStanding = findViewById(R.id.activityIndicatorIconStanding)
+        activityIndicatorIconRunning = findViewById(R.id.activityIndicatorIconRunning)
+
+        createNotificationChannel()
+        setClickListeners()
     }
 
+    private fun setClickListeners() {
+        // these listeners should be more modular...
+        activityIndicatorIconSitting.apply {
+            setOnTouchListener { v, event ->
+                val n = NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_airline_seat_recline_normal_24)
+                    .setContentTitle(context.getString(R.string.time_for_a_break))
+                    .setContentText(context.getString(R.string.sitting_activity_description))
+                    .setStyle(NotificationCompat.BigTextStyle())
+                with(NotificationManagerCompat.from(context)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(0, n.build())
+                }
+                performClick()
+                true
+            }
+        }
+        activityIndicatorIconStanding.apply {
+            setOnTouchListener { v, event ->
+                val n = NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_accessibility_new_24)
+                    .setContentTitle(context.getString(R.string.almost_there))
+                    .setContentText(context.getString(R.string.movement_activity_description))
+                    .setStyle(NotificationCompat.BigTextStyle())
+                with(NotificationManagerCompat.from(context)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(0, n.build())
+                }
+                performClick()
+                true
+            }
+        }
+        activityIndicatorIconRunning.apply {
+            setOnTouchListener { v, event ->
+                val n = NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
+                    .setContentTitle(context.getString(R.string.awesome_moves))
+                    .setContentText(context.getString(R.string.goal_reached_activity_description))
+                    .setStyle(NotificationCompat.BigTextStyle())
+                with(NotificationManagerCompat.from(context)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(0, n.build())
+                }
+                performClick()
+                true
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Activity alerts"
+            val descriptionText = "Shows activity reminders"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
 //    private fun init() {
 //    }
